83438b821a754c42eee803f5b7d615410da5ac7e
dhanesh <dhaneshprabhupro@gmail.com>
2025-08-20 23:21:23 +0530

    Intializing Project T Leap

--- tleap-frontend/src/App.jsx
+++ tleap-frontend/src/App.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import SubjectSelection from './components/SubjectSelection'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <SubjectSelection />
+    </div>
   )
 }
 
